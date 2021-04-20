01546ea1f627d6bd94c33fdeb4318bc5b28913ec
Carsten König <Carsten.Koenig@hotmail.de>
2021-04-20 15:18:48 +0200

    direkte Implementation in eigenes Modul

---
+++ src/DirecteImplementation.fs
@@ -0,0 +1,23 @@
+﻿module Nimm.Game.Direct
+
+open Nimm.Game
+
+let private askForNumber =
+    let console = DependencyInjection.Console()
+    let io = DependencyInjection.IO console
+    io.AskForNumber
+
+/// direkte Umsetzung
+let rec playGame state =
+    System.Console.Clear ()
+    printfn "NIMM die letzte Münze und Du hast gewonnen!"
+    printfn "===========================================\n\n"
+    printfn "%O" state
+    printfn "-------------------------------------------\n\n"
+    let actions = State.validActions state
+    if actions.IsEmpty then () else
+    for (i, action) in Seq.indexed actions do
+        printfn "(%d): %O" (i+1) action
+    let actionNumber = askForNumber actions.Length "Bitte Aktionsnummer eingeben:"
+    let state' = State.act actions.[actionNumber-1] state
+    playGame state'
--- src/Program.fs
+++ src/Program.fs
@@ -1,25 +1,9 @@
-﻿open System
-open Nimm.Game
+﻿open Nimm.Game
 
 
-/// direkte Umsetzung
-let rec playGame state =
-    System.Console.Clear ()
-    printfn "NIMM die letzte Münze und Du hast gewonnen!"
-    printfn "===========================================\n\n"
-    printfn "%O" state
-    printfn "-------------------------------------------\n\n"
-    let actions = State.validActions state
-    if actions.IsEmpty then () else
-    for (i, action) in Seq.indexed actions do
-        printfn "(%d): %O" (i+1) action
-    let actionNumber = FreeMonads.Console.askForNumber actions.Length "Bitte Aktionsnummer eingeben:"
-    let state' = State.act actions.[actionNumber-1] state
-    playGame state'
-
-let runGame coins =
+let runDirect coins =
     State.initWithCoins coins
-    |> playGame
+    |> Direct.playGame
 
 let runDependencyInjection =
     let console = new DependencyInjection.Console()
@@ -34,7 +18,7 @@ let runConsoleInterpreter coins =
 
 [<EntryPoint>]
 let main argv =
-    // runGame 13
-    runDependencyInjection 13 |> ignore
+    runDirect 13
+    // runDependencyInjection 13 |> ignore
     // runConsoleInterpreter 13 |> ignore
     0 // return an integer exit code
