140524804d22c8e48c0837d439ff8f7f9791ec1f
Carsten König <Carsten.Koenig@hotmail.de>
2021-04-22 15:17:57 +0200

    Agenda hinzugefügt

---
+++ src/Agenda.fsx
@@ -0,0 +1,53 @@
+
+(*
+                                    WW  WW                                      
+                                  WNO0WNXKKNW                                   
+                                WNOdlkWNKOdkKNW                                 
+                              WNOdlclkWNKOdddk0NW                               
+                            WNOdlccclkWNKOdddddk0NW                             
+                          WXOdlccccclkWNKkdddddddx0NW                           
+                        WNOdlccccccclkNNKOdddddddddx0XW                         
+                       NOdlccccccccclkNNKOdddddddddddx0XW                       
+                    WNOdlccccccccccclkWNKOdddddddddddddxOXW                     
+                  WNOdlcccccccccccccoOWNX0dddddddddddddddxOXW                   
+                WNOdlccccccccccccclxKW  WNKkdddddddddddddddxOXW                 
+              WNOdlccccccccccccclxKW       WKkdddddddddddddddxOXW               
+            WNOdlccccccccccccclxKW WKXW      NKkdddddddddddddddxOKW             
+          WXOdlccccccccccccclxKW WKxokW        NKkdddddddddddddddxkKN           
+        WNOdlccccccccccccclxKW WKxlclkW          NKkddddddddddddddddkKN         
+      WNOdlccccccccccccclxKW WKxlccclkW            NKkddddddddddddddddkKN       
+    WNOdlccccccccccccclxKW WKxlccccclkW              WKkddddddddddddddddkKN     
+  WXOdlccccccccccccclxKW WKxlccccccclkW                NKkddddddddddddddddk0NW  
+  NOlcccccccccccccclxX  WKdlcccccccclkW                 WXkddddddddddddddddx0W  
+   N0dlcccccccccccccld0N WXkoccccccclkW               WN0kddddddddddddddddkKN   
+     N0dlcccccccccccccld0N WXkoccccclkW              N0kdddddddddddddddxOKW     
+       N0dlcccccccccccccld0N WXkoccclkW            N0kdddddddddddddddxOXW       
+         N0dlcccccccccccccld0N WXkoclkW          N0kdddddddddddddddxOXW         
+           N0dlcccccccccccccld0NWWXkokW        N0kdddddddddddddddxOXW           
+            WN0dlcccccccccccccld0N WXXW     WN0kdddddddddddddddxOXW             
+               N0dlccccccccccccclx0N      WN0kdddddddddddddddx0XW               
+                 N0dlcccccccccccccld0N  WN0kdddddddddddddddx0XW                 
+                   N0dlccccccccccccclOWNXOdddddddddddddddx0NW                   
+                     N0dlccccccccccclkWNKOdddddddddddddk0NW                     
+                       N0dlccccccccclkWNKOdddddddddddk0NW                       
+                         N0dlccccccclkNNKOdddddddddkKNW                         
+                           N0dlccccclkWNKOdddddddkKN                            
+                            WN0dlccclkWNKOdddddkKNW                             
+                               N0dlclkWNXOdddkKW                                
+                                 N0dlkWNKOxOKW                                  
+                                   N0KWWXKXW                                    
+                                        W                                       
+
+                   Einblick in funktionale Programmierung und F#
+
+                                      Agenda
+*)
+
+// ---------- Einführung ------------
+
+// ---------- Let's Code ------------
+
+// ------ Dependency Injection ------
+
+// ---------- Das M-Wort ------------
+
